6941e0862d2d4e609533983bb91a95867d51ebca
leizi <60679461+Suzuran1480@users.noreply.github.com>
2021-05-22 13:05:56 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -22,7 +22,7 @@ sed -i '/uci commit system/i\uci set system.@system[0].hostname='OpenWrt'' packa
 
 # 版本号里显示一个自己的名字（ababwnq build $(TZ=UTC-8 date "+%Y.%m.%d") @ 这些都是后增加的）
 #sed -i "s/OpenWrt /ababwnq build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
-sed -i "s/OpenWrt / Suzuran1480 build $(TZ=UTC-8 date "+%Y.%m.%d") @ XiaoMi router 4A Gigabit /g" package/lean/default-settings/files/zzz-default-settings
+sed -i "s/OpenWrt / Suzuran1480 build $(TZ=UTC-8 date "+%Y.%m.%d") @ XiaoMi Router 4A Gigabit /g" package/lean/default-settings/files/zzz-default-settings
 
 
 #删除原默认主题
